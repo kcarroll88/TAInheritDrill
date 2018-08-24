6e9b702e475067af1782478badc14dd6818fcacd
Keenan Carroll <keenan.carroll.va@gmail.com>
2018-08-24 15:00:30 -0400

    Inherit Class test

---
+++ TAInheritDrill/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAInheritDrill
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void SayName()
+        {
+            string fullName = FirstName + LastName;
+            Console.WriteLine("Your full name is : {0}", fullName);
+        }
+    }
+}
--- TAInheritDrill/Program.cs
+++ TAInheritDrill/Program.cs
@@ -10,6 +10,13 @@ namespace TAInheritDrill
     {
         static void Main(string[] args)
         {
+            Employee employee = new Employee();
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+
+            employee.SayName();
+
+            Console.ReadLine();
         }
     }
 }
